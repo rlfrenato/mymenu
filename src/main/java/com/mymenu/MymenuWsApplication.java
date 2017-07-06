ec51b5b446ab9486b9852affdc6ef3c581ed642f
Renato Franceschinelli <rlf.renato@gmail.com>
2017-07-05 21:10:17 -0300

    Initial project setup

---
+++ src/main/java/com/mymenu/MymenuWsApplication.java
@@ -0,0 +1,12 @@
+package com.mymenu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymenuWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymenuWsApplication.class, args);
+	}
+}
